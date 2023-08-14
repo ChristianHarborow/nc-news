6ba148b57d54ca97974f4e143a3a929e2b229918
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-14 15:13:49 +0100

    adds /api/articles/:article_id endpoint

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const {getTopics} = require("./controllers/topics-controllers")
 const {getApiDetails} = require("./controllers/api-controllers")
-const {handleServerErrors} = require("./error-handlers")
+const {getArticleById} = require("./controllers/articles-controllers")
+const {handleCustomErrors, handleSqlErrors, handleServerErrors} = require("./error-handlers")
 const express = require("express")
 
 const app = express()
+
+app.use(express.json())
 
 app.get("/api", getApiDetails)
 
 app.get("/api/topics", getTopics)
 
+app.get("/api/articles/:article_id", getArticleById)
+
+app.use(handleCustomErrors)
+app.use(handleSqlErrors)
 app.use(handleServerErrors)
 
 module.exports = app
