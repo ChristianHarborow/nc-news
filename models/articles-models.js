bce708f5eb66cb021a6a5fab99961e2ded9b3651
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-14 15:10:41 +0100

    adds articles models file

---
+++ models/articles-models.js
@@ -0,0 +1,11 @@
+const db = require("../db/connection")
+
+exports.readArticleById = (articleId) => {
+    return db.query(`
+        SELECT * FROM articles
+        WHERE article_id = $1
+    `, [articleId])
+    .then(({rows}) => {
+        return rows[0]
+    })
+}
