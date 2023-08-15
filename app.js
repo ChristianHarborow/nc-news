9a628651e09673c02a1a5597e68eece7ff0b3d08
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-15 17:04:48 +0100

    adds update article votes endpoint

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const {getTopics} = require("./controllers/topics-controllers")
 const {getApiDetails} = require("./controllers/api-controllers")
-const {getArticles, getArticleById} = require("./controllers/articles-controllers")
+const {getArticles, getArticleById, patchArticleById} = require("./controllers/articles-controllers")
 const {getCommentsByArticleId} = require("./controllers/comments-controllers")
 const {handleCustomErrors, handleSqlErrors, handleServerErrors} = require("./error-handlers")
 const express = require("express")
 
 const app = express()
+
+app.use(express.json())
 
 app.get("/api", getApiDetails)
 
@@ -13,6 +15,7 @@ app.get("/api/topics", getTopics)
 
 app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id", getArticleById)
+app.patch("/api/articles/:article_id", patchArticleById)
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
