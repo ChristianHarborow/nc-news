76939748534b9ef4b221f225ce22058ca74218b2
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-15 11:05:21 +0100

    adds articles endpoint

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const {getTopics} = require("./controllers/topics-controllers")
 const {getApiDetails} = require("./controllers/api-controllers")
-const {getArticleById} = require("./controllers/articles-controllers")
+const {getArticles, getArticleById} = require("./controllers/articles-controllers")
 const {handleCustomErrors, handleSqlErrors, handleServerErrors} = require("./error-handlers")
 const express = require("express")
 
@@ -10,7 +10,12 @@ app.get("/api", getApiDetails)
 
 app.get("/api/topics", getTopics)
 
+app.get("/api/articles", getArticles)
 app.get("/api/articles/:article_id", getArticleById)
+
+app.use((req, res) => {
+    res.status(404).send({msg: "Not found"})
+})
 
 app.use(handleCustomErrors)
 app.use(handleSqlErrors)
