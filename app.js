771a2fc62d2fdff55df4cf768d948b32ad2fc86b
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-16 11:08:44 +0100

    adds delete comment by id endpoint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const {getTopics} = require("./controllers/topics-controllers")
 const {getApiDetails} = require("./controllers/api-controllers")
 const {getArticles, getArticleById} = require("./controllers/articles-controllers")
-const {getCommentsByArticleId, postComment} = require("./controllers/comments-controllers")
+const {getCommentsByArticleId, postComment, deleteComment} = require("./controllers/comments-controllers")
 const {handleCustomErrors, handleSqlErrors, handleServerErrors} = require("./error-handlers")
 const express = require("express")
 
@@ -18,6 +18,7 @@ app.get("/api/articles/:article_id", getArticleById)
 
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 app.post("/api/articles/:article_id/comments", postComment)
+app.delete("/api/comments/:comment_id", deleteComment)
 
 app.use((req, res) => {
     res.status(404).send({msg: "Not found"})
