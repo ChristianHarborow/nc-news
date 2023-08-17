6543588c57e953d96372b32df17cf8b25bfb1887
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-17 17:07:53 +0100

    refactors app.js to use routers

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const {getTopics} = require("./controllers/topics-controllers")
-const {getApiDetails} = require("./controllers/api-controllers")
-const {getArticles, getArticleById, patchArticleById} = require("./controllers/articles-controllers")
-const {getCommentsByArticleId, postComment, deleteComment} = require("./controllers/comments-controllers")
-const {getUsers} = require("./controllers/users-controllers")
+const apiRouter = require("./routes/api-router")
 const {handleCustomErrors, handleSqlErrors, handleServerErrors} = require("./error-handlers")
 const express = require("express")
 
@@ -10,19 +6,7 @@ const app = express()
 
 app.use(express.json())
 
-app.get("/api", getApiDetails)
-
-app.get("/api/topics", getTopics)
-
-app.get("/api/articles", getArticles)
-app.get("/api/articles/:article_id", getArticleById)
-app.patch("/api/articles/:article_id", patchArticleById)
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
-app.post("/api/articles/:article_id/comments", postComment)
-app.delete("/api/comments/:comment_id", deleteComment)
-
-app.get("/api/users", getUsers)
+app.use("/api", apiRouter)
 
 app.use((req, res) => {
     res.status(404).send({msg: "Not found"})
---
+++ routes/articles-router.js
@@ -0,0 +1,16 @@
+const {getArticles, getArticleById, patchArticleById} = require("../controllers/articles-controllers")
+const {getCommentsByArticleId, postComment} = require("../controllers/comments-controllers")
+const articlesRouter = require('express').Router()
+
+articlesRouter.route('/')
+    .get(getArticles)
+
+articlesRouter.route('/:article_id')
+    .get(getArticleById)
+    .patch(patchArticleById)
+
+articlesRouter.route('/:article_id/comments')
+    .get(getCommentsByArticleId)
+    .post(postComment)
+
+module.exports = articlesRouter
