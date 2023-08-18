75d03ea70b2c64a6bebd27c7f4c2c354839baa8a
ChristianHarborow <christianharborow@hotmail.co.uk>
2023-08-18 15:28:51 +0100

    adds postArticle endpoint

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -1,9 +1,10 @@
-const {getArticles, getArticleById, patchArticleById} = require("../controllers/articles-controllers")
+const {getArticles, getArticleById, patchArticleById, postArticle} = require("../controllers/articles-controllers")
 const {getCommentsByArticleId, postComment} = require("../controllers/comments-controllers")
 const articlesRouter = require('express').Router()
 
 articlesRouter.route('/')
     .get(getArticles)
+    .post(postArticle)
 
 articlesRouter.route('/:article_id')
     .get(getArticleById)
